58212f3962886dd6673a0c0ef2c5281d596a4495
Aya Shameimaru <noscoper4ver@gmail.com>
2016-10-08 14:42:17 -0700

    Create Messages!

--- code/Client.js
+++ code/Client.js
@@ -1,4 +1,4 @@
-const WebSocket = require("ws"), request = require('request'), Zlib = require("zlib");
+const WebSocket = require("ws"), request = require('request'), Zlib = require("zlib"), requestp = require('request-promise-any')
 const ws = new WebSocket('wss://gateway.discord.gg/')
 
 var EventEmitter, Promise;
@@ -10,11 +10,17 @@ try {
 }
 var url = ("https://discordapp.com/api")
 
+const User = require("./structs/User");
+const Message = require("./structs/Message")
+const Channel = require("./structs/Channel")
+
 
 /**
 * DA CLIENT
 * @extends EventEmitter
 * @prop {String} token
+* @prop {Number} startT
+* @prop {Number} uptime
 */
 class Client extends EventEmitter {
     
@@ -27,9 +33,11 @@ class Client extends EventEmitter {
         super();
         
         this.token = token;
+        this.startT = 0;
+        this.isReady = false;
+    
 		
-		request.get({ url: `${url}/gateway?encoding=json&v=6` }).on('response', function(response) { console.log(response.statusCode) })
-   
+		request.get({ url: `${url}/gateway?encoding=json&v=6` }).on('response', function(response) {  })
     }
 	connect() {
 
@@ -51,7 +59,9 @@ class Client extends EventEmitter {
 		            token: self.token
 	            }
             }))
+           this.startT = Date.now();
 		});
+		
 		
 		ws.on("message", function(data, flags) {
             if (flags.binary)
@@ -62,15 +72,51 @@ class Client extends EventEmitter {
                     this.seq = message.s;
     		switch(message.t) {
                 case "READY":
+                	self.emit("botReady")
+                	this.isReady = true;
                     this.heartbeatInterval = setInterval(()=>{
                         ws.send(JSON.stringify({
                             op: 1,
                             d: this.seq
                         }))
                     }, message.d.heartbeat_interval);
+                break;
+                case "MESSAGE_CREATE":
+                	self.emit("createdMessage", message.d)
+                	this.author = new User(message.d.author);
+                break;
+                case "GUILD_CREATE":
+                	self.emit("guildAdd", message.d)
+                break;
             }
 		});
-		
+	}
+	
+	get uptime() {
+	    return (this.startT ? Date.now() - this.startT : 0);
+	}
+
+	/**
+	* Make message in channel!
+	* @arg {String} channelID The ID of a channel
+	*/
+	
+	makeMessage(channelID, content) {
+		if (content == null) {
+			return;
+		}
+		let options = {
+    		method: 'POST',
+			uri: `${url}/channels/${channelID}/messages`,
+    		json: true, // Automatically stringifies the body to JSON
+    		form: {
+        		'content': `${content}`
+    		},
+    		headers: {
+        		'Authorization': `Bot ${this.token}`
+    		}
+		};
+		return requestp(options).then((message) => { new Message(message, this) });
 	}
 }
 
